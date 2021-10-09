3e3b27c36349d0d951da076abad3c7588acecbc1
binhcaong <binhcn@vng.com.vn>
2021-10-09 20:39:47 +0700

    update domain

--- frontend/src/utils/common.js
+++ frontend/src/utils/common.js
@@ -1,2 +1,3 @@
-export const DOMAIN = 'http://hoc-hoa-free.herokuapp.com/api'
+export const DOMAIN = 'http://ec2-18-141-224-159.ap-southeast-1.compute.amazonaws.com:8000/api'
+// export const DOMAIN = 'http://localhost:8000/api'
 export const FILE_DOMAIN = 'https://hoc-hoa-free.s3.ap-southeast-1.amazonaws.com/'
