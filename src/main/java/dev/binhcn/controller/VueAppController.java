3b10eb02606db445099caadb42e7c783dbe0af27
binhcaong <binhcn@vng.com.vn>
2021-10-10 16:26:57 +0700

    update path

---
+++ src/main/java/dev/binhcn/controller/VueAppController.java
@@ -0,0 +1,18 @@
+package dev.binhcn.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class VueAppController {
+
+    @RequestMapping(value = { "/", "/{x:[\\w\\-]+}", "/{x:^(?!api$).*$}/**/{y:[\\w\\-]+}" })
+    public String getIndex(HttpServletRequest request) {
+        return "/index.html";
+    }
+
+}
