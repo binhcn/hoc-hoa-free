00bd12778cc9c923f90a28245ce9111b2da6bc3e
binhcaong <binhcn@vng.com.vn>
2021-10-30 09:08:23 +0700

    update code

--- frontend/src/utils/common.js
+++ frontend/src/utils/common.js
@@ -1,3 +1,4 @@
 export const DOMAIN = 'https://hochoafree.com/api'
+// export const DOMAIN = 'http://3.1.85.236/api'
 // export const DOMAIN = 'http://localhost:8000/api'
 export const FILE_DOMAIN = 'https://hoc-hoa-free.s3.ap-southeast-1.amazonaws.com/'
