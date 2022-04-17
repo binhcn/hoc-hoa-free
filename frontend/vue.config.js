4561de0d699918831eae26047479a1cc1c09fb4f
HungNT <hunghop0909@gmail.com>
2022-04-17 17:25:24 +0700

    add package

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,14 +1,20 @@
+const path = require("path");
+const PrerenderSPAPlugin = require("prerender-spa-plugin");
+
 module.exports = {
-  // devServer: {
-  //   proxy: {
-  //     '/api': {
-  //       target: 'http://localhost:8080',
-  //       ws: true,
-  //       changeOrigin: true
-  //     }
-  //   }
-  // },
+  configureWebpack: {
+    plugins: [
+      new PrerenderSPAPlugin({
+        // Required - The path to the webpack-outputted app to prerender.
+        staticDir: path.join(__dirname, "dist"),
+        // Required - Routes to render.
+        routes: [
+          "/",
+        ],
+      }),
+    ],
+  },
   lintOnSave: false,
-  outputDir: 'dist',
-  assetsDir: 'static'
-}
+  outputDir: "dist",
+  assetsDir: "static",
+};
